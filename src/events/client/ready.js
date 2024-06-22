409b54fcbb93d41763cb24263d4d6b5031c45b55
OsamaNaim85 <osamaneim85@gmail.com>
2024-06-22 05:10:43 +0300

    ONeity Quran Bot For ONeity Community Server

---
+++ src/events/client/ready.js
@@ -0,0 +1,49 @@
+const joinAndPlayQuran = require('@root/src/utils/functions/joinAndPlayQuran');
+const chalk = require('chalk');
+const { ActivityType, } = require('discord.js');
+const gr = chalk.hex('#00D100');
+const un = chalk.underline;
+
+module.exports = {
+  name: 'ready',
+  /**
+   * @param {import("@base/baseClient")} client 
+   */
+  async execute(client) {
+
+    await client.DBConnect();
+    await client.registerInteractions();
+
+    const commands = client.slashCommands.map(({ execute, ...data }) => data);
+    setTimeout(() => {
+      console.log(gr(`Logged In As ` + un(`${client.user.username}`)));
+      console.log(chalk.cyan(`Servers:` + un(`${client.guilds.cache.size}`)), chalk.red(`Users:` + un(`${client.users.cache.size}`)), chalk.blue(`Commands:` + un(` ${client.commands.size}` + ` TOTAL Commands ${client.commands.size + commands.length}`)));
+    }, 3000);
+    client.user.setStatus("idle")
+    client.user.setActivity({ name: `Loading....`, type: ActivityType.Playing })
+    setTimeout(() => client.user.setStatus("online"), 40000);
+    setInterval(() => {
+      let ServersStatus = client.Radio.size
+      client.user.setActivity({ name: `in ${ServersStatus} Server`, type: ActivityType.Listening })
+    }, 1 * 1000 * 60);
+
+    let RadioChannels = await client.db.table("channels").values() || [];
+    if (RadioChannels.length === 0) return
+    setTimeout(async () => {
+
+      for (let data of RadioChannels) {
+        if (data.enabled) {
+
+          let guild = await client.guilds.fetch(data.guildId)
+          if (!guild) continue
+          let conn = await joinAndPlayQuran(client, data.channelId, guild, data.url)
+          if (conn === null) continue
+          if (conn === "cantConnect") continue
+          client.Radio.set(data.guildId, conn)
+        }
+
+      }
+    }, 1000);
+
+  },
+};
---
+++ src/utils/functions/joinAndPlayQuran.js
@@ -0,0 +1,79 @@
+const fetch = require('node-fetch');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const ControlData = require('./ControlData');
+let radioUrlCache = new Map();
+
+/**
+ * 
+ * @param {import("@base/baseClient")} client 
+ * @param {import("discord.js").Channel} channel 
+ * @param {import("discord.js").Guild} guild 
+ * @returns 
+ */
+module.exports = async function joinAndPlayQuran(client, channelId, guild, url = process.env.RadioAudioUrl, isRunig = false) {
+
+  const guildId = guild.id;
+  radioUrlCache.set(guildId, url)
+  const channel = await guild.channels.fetch(channelId).catch(() => null);
+
+  if (!channel) return null;
+  if (isRunig) {
+
+    client.Radio.get(guildId).player.stop(true);
+    const response = await fetch(url)
+    const stream = response.body;
+
+    const resource = createAudioResource(stream);
+
+    client.Radio.get(guildId).player.play(resource);
+    return "isRunig"
+  }
+
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guildId,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+
+  if (!connection) return "cantConnect";
+
+  const player = createAudioPlayer();
+  const response = await fetch(url)
+
+  const stream = response.body;
+
+  const resource = createAudioResource(stream);
+  player.play(resource);
+
+  connection.subscribe(player);
+  connection.on("stateChange", async (state) => {
+    // this if condition disconnected bots 
+    if (state.subscription.connection.state.status == "disconnected") {
+
+      let guildId = state.subscription.connection.joinConfig.guildId
+      let guildd = client.guilds.cache.get(guildId)
+      if (guildd.members.me.voice?.channelId !== null) return
+
+      await client.db.table("channels").set(`${guildId}_radioChannel..enabled`, false)
+      let data = await client.db.table("channels").get(`${guildId}_radioChannel`)
+      let msg = await guildd.channels.cache.get(data.ch).messages.fetch(data.msgId)
+
+      if (msg) msg?.edit(ControlData(client, data))
+      state.subscription.player.stop(true);
+      client.Radio?.delete(guildId)
+      console.log(`Bot disconnected from server ${guildd.name}`);
+
+    }
+  })
+  player.on(AudioPlayerStatus.Playing, () => console.log(`Playing Quran in Server: [${guild.name}] Channel: [${channel.name}] ${new Date()}`));
+  player.on('error', async (error) => {
+    let radioUrl = radioUrlCache.get(guildId)
+    setTimeout(async function () {
+      await joinAndPlayQuran(client, channelId, guild, radioUrl, true)
+    }, 1000);
+    console.error(`${error.message} Server : ${guild.name} and rest Radio in :${channel.name} - url: ${radioUrl}`);
+  })
+  connection.player = player
+  return connection
+
+}
